c40ce77e2eb77f149275e3aef17f6917dca4c7ba
rebeccakhachaya <rebeccakhachaya@gmail.com>
2021-08-12 06:56:38 +0300

    first

--- run.py
+++
@@ -1,8 +0,0 @@
-from bank import MobileMoneyAccount
-
-user = MobileMoneyAccount("Becky","757wqt7e9","Safaricom")
-user2 = MobileMoneyAccount("Vero","555587","Safaricom")
-
-print(user.deposit(5300))
-print(user.buy_airtime(40,user2))
-print(user.statement())
--- student.py
+++
@@ -1,7 +0,0 @@
-class Student:
-    school="Akirachix"
-    def __init__(self,name,age):
-        self.name=name
-        self.age=age
-    def speak(self):
-        return f"Hello my name is {self.name},I am {self.age} years old I love {self.school}."
